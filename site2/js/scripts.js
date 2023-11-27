042aa04d214a4da4975787f46caab1383087d216
annaleonard <ajl117@students.jefferson.edu>
2023-11-26 22:06:07 -0500

    commit

--- site2/js/scripts.js
+++ site2/js/scripts.js
@@ -1,30 +1,40 @@
-document.addEventListener("DOMContentLoaded", function() {  
-  /*global fetch, response*/
-fetch("partials/header.html")
-    .then(response => {
-    return response.text()
-  })
-  .then(data => {
-    document.querySelector("header").innerHTML = data;
-    navLinks = document.querySelectorAll(".nav-link");
-console.log(navLinks);     
-// Loop through each anchor element
-navLinks.forEach((link) => {
-  // Check if the href property of the anchor element matches the current page's URL
-  if (link.href === location.href) {
-    // Add the "active" class to the anchor element
-    link.classList.add("active");
-  } else {
-    // Remove the "active" class from the anchor element
-    link.classList.remove("active");
-  }
-});
-    
-    
-  });  
-});      
 
-let today = new Date();
-let year = today.getFullYear();
-// TASK: Set the inner html of the <p> with an id of footer-notice to this: Copyright and then use a template literal to show the year variable.
-document.getElementById("footer-notice").innerHTML=`<p>Copyright &copy; ${year}</p>`
+document.addEventListener("DOMContentLoaded", function() {  
+    /*global fetch, response*/
+    fetch("partials/header.html")
+        .then(response => {
+        return response.text()
+      })
+      .then(data => {
+        document.querySelector("header").innerHTML = data;
+        navLinks = document.querySelectorAll(".nav-link");
+        console.log(navLinks);     
+        // Loop through each anchor element
+        navLinks.forEach((link) => {
+            // Check if the href property of the anchor element matches the current page's URL
+            if (link.href === location.href) {
+            // Add the "active" class to the anchor element
+            link.classList.add("active");
+        } else {
+            // Remove the "active" class from the anchor element
+            link.classList.remove("active");
+        }
+        });
+    });
+}); 
+
+document.addEventListener("DOMContentLoaded", function() {  
+    let today = new Date();
+    //console.log(today);
+    let year = today.getFullYear();
+    //console.log(year);
+    /*global fetch, response*/
+    fetch("partials/footer.html")
+        .then(response => {
+        return response.text();
+      })
+      .then(data => {
+        document.querySelector("footer").innerHTML = data;
+        document.querySelector("footer").innerHTML = document.querySelector("footer").innerHTML.replace('${year}', year);
+        });
+});
